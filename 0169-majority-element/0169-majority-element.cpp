df0d8157fac9abb023b9bdc16abecf108d7587ad
Ghanshyam Bagadi <115298453+Ghanshyam444@users.noreply.github.com>
2024-06-09 18:20:37 +0530

    Time: 10 ms (95.85%), Space: 26.5 MB (90.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        map<int,int> mpp;
-        for(int i=0;i<n;i++)
-            mpp[nums[i]]++;
-        for(auto x:mpp)
+        int cnt=0,el;
+        for(int i=0;i<nums.size();i++)
         {
-            if(x.second > n/2) return x.first;
+            if(cnt == 0)
+            {
+                el = nums[i];
+                cnt = 1;
+            }
+            else if(nums[i] == el) cnt++;
+            else
+                cnt--;
         }
+        //as in problem  not stated that there is compulsarily a majority element present so we have to check it
+        int cnt0 = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==el) cnt0++;
+        }
+        if(cnt0 > nums.size()/2) return el;
         return -1;
     }
 };
